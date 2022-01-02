145b73b7126cd7f2a2a6e521af8d75b9f6d3b71c
Yael Geisler <yaeli21995@gmail.com>
2022-01-02 16:00:00 +0200

    xx

--- dealer.py
+++ dealer.py
@@ -2,20 +2,17 @@ import random
 from math import log2
 from node import Node
 from scipy.interpolate import lagrange
-from shared import PLAYERS, DEALER
+from shared import PLAYERS
 from player import Player
 
 
 class Dealer:
-    def __init__(self, num_of_players):
-        self.__num_of_players = num_of_players
-        self.__random_X_vals = self.create_random_X_vals()
+    def __init__(self, num_of_PLAYERS):
+        self.__num_of_PLAYERS = num_of_PLAYERS
 
-    def create_random_X_vals(self):
-        return list(random.sample(range(1, 50), self.__num_of_players))
 
     def get_random_X_vals(self):
-        return self.__random_X_vals
+        return list(random.sample(range(1, 50), self.__num_of_PLAYERS))
 
     def get_sec_val_sum(self, X, PX):
         return int(round(lagrange(X, PX)(0)))
@@ -41,61 +38,66 @@ class Dealer:
         if len(C) == 1:
             # first cond: check if all objects in O have the same category ci
             # we do it by calc get_c_sum in each player for each category.
-            # if for a specific category ci all players return that get_c_sum(ci) == curr_db length
+            # if for a specific category ci all PLAYERS return that get_c_sum(ci) == curr_db length
             #  => if true -> return ci
             node.value = C[0];
             return node;
         if (len(R) == 0):
             # second condition
             # Return a leaf node whose category is set to the dominant category among the objects in O
-            # we calc the dominant by using secret sum af all players get_c_sum
+            # we calc the dominant by using secret sum af all PLAYERS get_c_sum
             node.value = self.find_max_category(node.attrs);
-            return node;
+            return node
 
         # else -  Determine the attribute A that best classifies the objects in O
         # and assign it as the test attribute for the current tree node
-        # ask from each player to calc Ta and Tac and sum the other players values by SSS
+        # ask from each player to calc Ta and Tac and sum the other PLAYERS values by SSS
         # the dealer will find the sum of the ss and find the optimal A using calc_E_TA.
-        # it will then send it back to all players -> to divide their data accordingly
+        # it will then send it back to all PLAYERS -> to divide their data accordingly
         # then - Create a new node for every possible value ai of A
         # and recursively call this method on it with R0 = (R - {A}) and O' = O(ai) /*
-        Ta = []
-        Tac = [[]]
+        Ta = {}
+        Tac = {}
         max_attr = {'attr': '', 'value': 0}
         for attr in R:
             for ai in attr:
-                Ta[ai] = self.get_Tai(node.attrs, ai)
+                Ta[ai] = self.get_Tai(node.attrs, attr, ai)
                 for ci in C:
                     Tac[ai][ci] = self.get_Tai_ci(node.attrs, ai, ci)
             curr_E_TA = self.calc_E_TA(self, R, Ta, Tac)
             # max_attr update
 
-
     def get_Tai(self, attrs, ai):
         X_vals = self.get_random_X_vals()
         enc_Tai = []
-        for i in range(len(players)):
-            enc_Tai[i] = players[i].get_Tai_sum(attrs, ai, X_vals[i])
+        for i in range(len(PLAYERS)):
+            enc_Tai[i] = PLAYERS[i].get_Tai_sum(attrs, ai, X_vals[i])
         return self.get_sec_val_sum(X_vals, enc_Tai)
 
     def get_Tai_ci(self, attrs, ai, ci):
         X_vals = self.get_random_X_vals()
         enc_Tai_ci = []
-        for i in range(len(players)):
-            enc_Tai_ci[i] = players[i].get_Tai_ci_sum(attrs, ai, ci, X_vals[i])
-        return self.get_sec_val_sum(X_vals, enc_Tai_ci)    
+        for i in range(len(PLAYERS)):
+            enc_Tai_ci[i] = PLAYERS[i].get_Tai_ci_sum(attrs, ai, ci, X_vals[i])
+        return self.get_sec_val_sum(X_vals, enc_Tai_ci)
 
+    def predict(self, attrs):
+        return self.__predict(self.tree, attrs)
 
+    def __predict(self, node, attrs):
+        if len(node.children) == 0:
+            return node.value
+        return self.__predict(node.children[attrs[node.value]], attrs)
 
     def main_loop(self):
         pass
 
 
 def main():
-    DEALER = Dealer(4)
+    dealer = Dealer(4)
     for i in range(4):
         PLAYERS.append(Player(i, 4))
-    DEALER.main_loop()
+    dealer.main_loop()
     print("Server created")
 
 
--- node.py
+++ node.py
@@ -1,8 +1,8 @@
 class Node:
     """Contains the information of the node and another nodes of the Decision Tree."""
 
-    def __init__(self, attrs, test):
+    def __init__(self, attrs, value):
         self.attrs = attrs
-        self.test = test
+        self.value = value
         self.next = None
         self.children = None
